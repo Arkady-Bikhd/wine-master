e9431107f4fb5025912704861adce0a75326385b
Arkadiy <capark74@mail.ru>
2023-02-11 19:18:06 +0300

    Создан парсер

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import datetime
 import pandas as pd
 from collections import defaultdict
 from pprint import pprint
+import argparse
 
 
-def get_wine_description():
+def get_wine_description(wine_filename):
    
-    wine_description = pd.read_excel('wine3.xlsx', engine='openpyxl', keep_default_na=False)
+    wine_description = pd.read_excel(wine_filename, keep_default_na=False)
     wine_description = wine_description.to_dict(orient='records')    
     commodity_group = defaultdict(list)     
     for commodity in wine_description:       
@@ -27,11 +28,13 @@ def get_declension(year):
     return "лет"
 
 def main():
+    
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
+    wine_filename = create_parser()
 
     template = env.get_template('template.html')
     foundation_year = 1920
@@ -40,7 +43,7 @@ def main():
     rendered_page = template.render(
         checked_time = time_duration,
         years_stamp = get_declension(time_duration),
-        wine_description = get_wine_description()   
+        wine_description = get_wine_description(wine_filename)   
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
@@ -51,6 +54,16 @@ def main():
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
 
+
+def create_parser():
+     
+    parser = argparse.ArgumentParser(
+        description='Новое русское вино',               
+    )
+    parser.add_argument('-wf','--wfile', default='wine.xlsx', help='Введите название файла')
+    args = parser.parse_args()
+    return args.wfile
+    
 
 if __name__ == '__main__':
 
